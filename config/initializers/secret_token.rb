c41a27b139ea950c14fcb0f19fd19a0b39d14aca
Jarrod Carlson <jarrod.carlson@gmail.com>
2013-09-03 19:10:06 -0400

    Initial commit
    $ rails new jmvc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jmvc::Application.config.secret_token = '6f346d25cb17dd77019a24c8a0bad54d577e74db97335b077b7d453f29064fde040625e70d5415a0e76bc76bbb6c92ec62d96a26888da52bb08c75e0af2c8dbc'
